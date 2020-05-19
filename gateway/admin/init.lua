f6129647993cdd19a89d77925b4ed7ecfda428a0
FengJianxin <fengjianxin2012@gmail.com>
2020-05-19 18:09:34 +0800

    lint: 解决 check-lua-code-style 问题

--- gateway/admin/init.lua
+++ gateway/admin/init.lua
@@ -14,6 +14,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
+local require = require
 local get_var = require("resty.ngxvar").fetch
 local ngx = ngx
 local get_method = ngx.req.get_method
--- gateway/admin/plugins.lua
+++ gateway/admin/plugins.lua
@@ -14,7 +14,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
-
+local ngx = ngx
 local ctx = require("app.core.ctx")
 local core_table = require("app.core.table")
 local resp = require("app.core.response")
