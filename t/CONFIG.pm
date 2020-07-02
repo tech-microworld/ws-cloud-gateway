dbc288b7d6bb5193ede446b03ddd12f243daa85a
FengJianxin <fengjianxin2012@gmail.com>
2020-07-02 20:40:59 +0800

    fix: license-check

--- t/CONFIG.pm
+++ t/CONFIG.pm
@@ -44,7 +44,7 @@ add_block_preprocessor(sub {
     lua_shared_dict upstream_type_cache 100k;
     lua_shared_dict timer_lock 100k;
     lua_shared_dict lrucache_lock 100k;
-    
+
 
     lua_package_path "$app_home/deps/share/lua/5.1/?.lua;$app_home/deps/share/lua/5.1/?/init.lua;$app_home/gateway/?.lua;$app_home/gateway/?/init.lua;$app_home/t/?.lua;;";
     lua_package_cpath "$app_home/deps/lib64/lua/5.1/?.so;$app_home/deps/lib/lua/5.1/?.so;;";
