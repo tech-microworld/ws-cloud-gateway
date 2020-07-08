80322130e31cacda48479e7707474911f761a94a
FengJianxin <fengjianxin2012@gmail.com>
2020-07-08 17:42:15 +0800

    log: 修改日志级别

--- gateway/app/core/balancer.lua
+++ gateway/app/core/balancer.lua
@@ -52,7 +52,7 @@ local balancer_types = {
 -- 刷新服务节点缓存
 local function refresh(service_name, nodes)
     local type = get_upstream_type(service_name)
-    log.error("refresh balancer: ", json.delay_encode({service_name, type, nodes}))
+    log.info("refresh balancer: ", json.delay_encode({service_name, type, nodes}))
     local balancer_up = balancer_types[type](nodes)
     return balancer_cache:set(service_name, balancer_up)
 end
